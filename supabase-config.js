ee79855bb0689464b45bcc33069a062a1b5a0771
yakiryakirh <yakirhasid@gmail.com>
2026-08-24 04:01:25 +0300

    Add public Supabase database config

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.KAIOKEN_DATABASE_CONFIG = {
+  supabaseUrl: "https://rxjsdnbixzsxvbzqmmki.supabase.co",
+  supabasePublishableKey: "sb_publishable_7LECpzo7gjrvZeZ83hJDKQ_hX5qFPBp",
+  redirectUrl: window.location.origin + window.location.pathname
+};
